724237e6aea07e64c18b5820c3ceb389629c6c50
Gabriel Watanabe <gabriel.yujip@gmail.com>
2021-02-21 10:42:39 -0300

    k

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost:27017/cliente_db?authSource=admin"
+    url: "mongodb+srv://deploy:uploadDeploy@cluster0.hbdok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   };
